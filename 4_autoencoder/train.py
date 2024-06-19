8a3ec21c55aa2955247c7c72d08c7fe844a3cb33
pep0 <baschy@msn.com>
2024-06-19 12:30:15 +0200

    chore: add train.py

---
+++ 4_autoencoder/train.py
@@ -0,0 +1,30 @@
+from auto_encoder import Autoencoder
+import keras
+
+LEARNING_RATE = 0.0005
+BATCH_SIZE = 32
+EPOCHS = 20
+
+def load_mnist():
+    (x_train, y_train), (x_test, y_test) = keras.mnist.load_data()
+
+    '''Add normalization and reshape, add extra dimension'''
+    x_train = x_train.astype("float32") / 255
+    x_train = x_train.reshape(x_train.shape, (1,))
+    x_test = x_test.astype("float32") / 255
+    x_test = x_test.reshape(x_test.shape, (1,))
+
+    return x_train, y_train, x_test, y_test
+
+def train(x_train, learning_rate, batch_size, epochs):
+    autoencoder = Autoencoder(
+        input_shape=(28, 28, 1),
+        conv_filters=(32, 64, 64, 64),
+        conv_kernels=(3, 3, 3, 3),
+        conv_strides=(1, 2, 2, 1),
+        latent_space_dim=2,
+    )
+
+if __name__ == "__main__":
+    x_train, _, _, _ = load_mnist()
+    autoencoder = train(x_train, LEARNING_RATE, BATCH_SIZE, EPOCHS)
