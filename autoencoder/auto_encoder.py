5c4555e533116dcc99830af4b1a9a76dcf920ead
pep0 <baschy@msn.com>
2024-06-19 14:44:38 +0200

    chore: add save and load functionality for model

--- autoencoder/auto_encoder.py
+++ autoencoder/auto_encoder.py
@@ -4,6 +4,9 @@ import tensorflow.keras as keras
 from tensorflow.keras import backend as K
 import numpy as np
 
+import os
+import pickle
+
 
 class Autoencoder:
     """
@@ -44,6 +47,46 @@ class Autoencoder:
         self.model.fit(
             x_train, x_train, batch_size=batch_size, epochs=num_epochs, shuffle=True
         )
+
+    def save(self, dir="."):
+        self._create_folder_if_it_doesnt_exist(dir)
+        self._save_parameters(dir)
+        self._save_weights(dir)
+
+    def load_weights(self, weights_file):
+        self.model.load_weights(weights_file)
+
+    @classmethod
+    def load(cls, dir="."):
+        parameters_file = os.path.join(dir, "parameters.pkl")
+        weights_file = os.path.join(dir, "autoencoder.weights.h5")
+
+        with open(parameters_file, "rb") as f:
+            parameters = pickle.load(f)
+
+        autoencoder = Autoencoder(*parameters)
+        autoencoder.load_weights(weights_file)
+        return autoencoder
+
+    def _create_folder_if_it_doesnt_exist(self, dir):
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+
+    def _save_parameters(self, dir):
+        parameters = [
+            self.input_shape,
+            self.conv_filters,
+            self.conv_kernels,
+            self.conv_strides,
+            self.latent_space_dim
+        ]
+        parameters_file = os.path.join(dir, "parameters.pkl")
+        with open(parameters_file, "wb") as f:
+            pickle.dump(parameters, f)
+
+    def _save_weights(self, dir):
+        weights_file = os.path.join(dir, "autoencoder.weights.h5")
+        self.model.save_weights(weights_file)
 
     def _build(self):
         self._build_encoder()
